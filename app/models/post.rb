6aa04cc510669f62792b676a4d42ad54387da6ab
Kyle <kylepcole@gmail.com>
2019-12-02 20:47:28 -0500

    lab passing

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,26 +1,24 @@
 class Post < ActiveRecord::Base
-	validates :title, presence: :true
+	validates :title, presence: true
 	validates :content, length: { minimum: 250, maximum: 500 }
 	validates :summary, length: { maximum: 250, minimum: 0 }
-	validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
-	# syntax the same as:
-	# validates :category, inclusion: { in: ["Fiction", "Non-Fiction"]}
+	validates :category, inclusion: { in: ["Fiction", "Non-Fiction"]}
 	validate :is_clickbait?
 
-	SPAM_FILTER = [/Won't Believe/,/Secret/,/Top [1-9]/,/Guess/]
+	# SPAM_FILTER = ["Won't Believe","Secret","Top","Guess"]
+	SPAM_FILTER = [/Won't Believe/i,/Secret/i,/Top [1-9]/i,/Guess/i]
 
-	# def is_clickbait?
-	# 	# if Post.include?(SPAM_FILTER)
-	# 		# binding.pry
-	# 	# if SPAM_FILTER.present?
 
-	# 	SPAM_FILTER.each do |regex|			
-	# 		if self.title.match(regex)
-	# 			errors.add(:title, "Sorry, this looks like clickbait")
-	# 			# binding.pry
-	# 		end
-	# 	end
-	# end
+	def is_clickbait?
+		if 
+			SPAM_FILTER.none? do |content|
+				# self.title.downcase.include?(content.downcase)
+				content.match(self.title)
+			end
+			
+			errors.add(:title, "must be clickbait")
+		end
+	end
 
 
 end
